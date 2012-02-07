a7ba93c7c6a1b44aee3961dfbe62bb5b13aa85a8
Kyle Stone <kylenstone@gmail.com>
2012-02-07 00:28:40 -0500

    Changed config/application.rb asset pipeline to false

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Gmaps::Application.config.secret_token = 'd760e306c750758c7803980d602a96ce710c0d7fc831f780c6b6418ff3652642f9bb4d52dfb84f657f3071795b1b0f562843f5019c52ec4ede08d2a9dbaaa633'
